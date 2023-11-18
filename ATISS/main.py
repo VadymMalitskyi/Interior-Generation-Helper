19356049862afc81d066050f65d147bc7a944c60
VadymMalitskyi <vadym.malitsky357@gmail.com>
2023-11-18 02:30:55 +0200

    Added loading reference room layout image to gradio and backend

--- ATISS/main.py
+++ ATISS/main.py
@@ -1,23 +1,59 @@
 import subprocess
 import time
+import io
+import pickle
+import random
 
-from fastapi import FastAPI, File, UploadFile, HTTPException
+import numpy as np
+from PIL import Image
+
+from fastapi import FastAPI, File, HTTPException, Form
 from fastapi.responses import FileResponse
 from pathlib import Path
-from pydantic import BaseModel
+from typing_extensions import Annotated
 
 app = FastAPI()
 
-class GenerationConfig(BaseModel):
-    style: str
-    room_id: str
-    required_objects: str
-    room_type: str
+bedroom_saved_info = {
+    "images_path": "/home/vadym_wsl/projects/Interior-Generation-Helper/images_list.pkl",
+    "names_path": "/home/vadym_wsl/projects/Interior-Generation-Helper/names_list.pkl",
+}
+livingroom_saved_info = {  # TODO: fill later
+    "images_path": "",
+    "names_path": "",
+}
 
-@app.post("/generate_bedroom")
-def generate_room(generation_config: GenerationConfig):  # file: UploadFile = File(...)
+def get_room_id(image_arr: np.ndarray, room_type: str) -> str:
+    existing_rooms_info = bedroom_saved_info if room_type == "bedroom" else livingroom_saved_info
+    with open(existing_rooms_info["images_path"], "rb") as f:
+        images = pickle.load(f)
+
+    with open(existing_rooms_info["names_path"], "rb") as f:
+        names = pickle.load(f)
+    
+    for image, name in zip(images, names):
+        print(image.shape)
+        print(image)
+        if np.array_equal(image, image_arr):
+            print("Found image id: ", name)
+            return name
+    print("Image id not found")
+    return random.sample(names, 1)[0]
+
+@app.post("/generate_room")
+def generate_room(
+    room_layout: Annotated[bytes, File()],
+    style: Annotated[str, Form()],
+    room_type: Annotated[str, Form()],
+    required_objects: Annotated[str, Form()],
+    layout_width: Annotated[int, Form()],
+    layout_height: Annotated[int, Form()],
+):
     try:
-        if generation_config.room_type == "bedroom":
+        layout_image = Image.frombytes("RGB", (layout_width, layout_height), io.BytesIO(room_layout).read())
+        room_id = get_room_id(image_arr=np.array(layout_image), room_type=room_type)
+        print("Image shape: ", np.array(layout_image).shape)
+        if room_type == "bedroom":
             weights_path = "/home/vadym_wsl/projects/Interior-Generation-Helper/ATISS/trained_models/L1F6VFKD0/model_01000"
         else:
             weights_path = None # TODO: add weights for living room
@@ -35,13 +71,12 @@ def generate_room(generation_config: GenerationConfig):  # file: UploadFile = Fi
             "--weight_file", weights_path,
             "--n_sequences", "1",
             "--without_screen",
-            "--scene_id", generation_config.room_id,
+            "--scene_id", room_id,
             "--file_save_name", file_name,
-            "--required_style", generation_config.style,
-            "--required_objects", generation_config.required_objects,
+            "--required_style", style,
+            "--required_objects", required_objects,
         ]
 
-        # Run the script with subprocess
         subprocess.run(command, check=True)
 
     except subprocess.CalledProcessError as e:
--- gradio_app/main.py
+++ gradio_app/main.py
@@ -1,12 +1,8 @@
-import gradio as gr
-import os
 import json
 import requests
 import time
-import hashlib
-import random
 
-import numpy as np
+import gradio as gr
 from PIL import Image
 
 
@@ -14,55 +10,37 @@ bedroom_objects = ["armchair", "bookshelf", "cabinet", "ceiling_lamp", "chair", 
                    "coffee_table", "desk", "double_bed", "dressing_chair", "dressing_table", "kids_bed",
                    "nightstand", "pendant_lamp", "shelf", "single_bed", "sofa", "stool", "table",
                    "tv_stand", "wardrobe"]
+livingroom_objects = []  # TODO: fill later
 
-bedroom_hash_to_room = {
-    "a700a745063abfb9cd01b37f9c63ef68fc578b78304bc8f16b939373218e3339": "SecondBedroom-18509",
-    "1dda516350c4f74caf1f24342680932b5b4f4ae1247fe03c9cb31fd0b25173c3": "MasterBedroom-2888",
-    "1fe1977cc02f6de5447e614540001e7a0c380d73779c833fe580eb5097d43d83": "SecondBedroom-60495",
-    "6a1853f2b4d90244a12ce11cf61944a4e6c87c237b13ad3f31b27e6e05075631": "MasterBedroom-29735",
-    "d51baebb753c66f156246cbb063ddfba5c7b9c9216b93ced6bb580f29f889ead": "MasterBedroom-81302",
-}
-
-livingroom_hash_to_room = {  # TODO: add living room hashes
-}
-
-# Function to download and save the 3D model
-def download_3d_model(save_path, style, room_type, room_id, objects):
-    json = {"style": style, "room_id": room_id, "room_type": room_type, "required_objects": objects}
-    response = requests.post("http://127.0.0.1:8000/generate_room", json=json)
+def download_3d_model(save_path, style, room_type, objects, room_layout, layout_width, layout_height):
+    json = {
+        "style": style, 
+        "room_type": room_type, 
+        "required_objects": objects, 
+        "layout_width": layout_width,
+        "layout_height": layout_height,
+    }
+    response = requests.post(
+        "http://127.0.0.1:8000/generate_room", 
+        data=json, 
+        files={"room_layout": room_layout}
+    )
     if response.status_code == 200:
         with open(save_path, 'wb') as f:
             f.write(response.content)
         return True
     return False
 
-# Gradio interface function
-def visualize_3d_model(room_layout, room_type, style, n_double_bed, n_wardrobe, n_pendant_lamp, n_ceiling_lamp, n_tv_stand, n_nightstand):
-    shape = room_layout.shape
-    tuple_list = []
+def visualize_3d_bedroom(room_layout, style, n_double_bed, n_wardrobe, n_pendant_lamp, n_ceiling_lamp, n_tv_stand, n_nightstand):
+    return visualize_3d_model(room_layout, style, n_double_bed, n_wardrobe, n_pendant_lamp, n_ceiling_lamp, n_tv_stand, n_nightstand, room_type="bedroom")
 
-    # Iterate through the array and convert it to a tuple of tuples
-    for i in range(shape[0]):
-        for j in range(shape[1]):
-            tuple_list.append(tuple(room_layout[i, j]))
+def visualize_3d_livingroom(room_layout, style, n_double_bed, n_wardrobe, n_pendant_lamp, n_ceiling_lamp, n_tv_stand, n_nightstand):
+    return visualize_3d_model(room_layout, style, n_double_bed, n_wardrobe, n_pendant_lamp, n_ceiling_lamp, n_tv_stand, n_nightstand, room_type="living_room")
 
-    # Convert the list of tuples to a tuple
-    result_tuple = tuple(tuple_list)
-    h = hashlib.new('sha256')#sha256 can be replaced with diffrent algorithms
-    h.update(str(result_tuple).encode()) #give a encoded string. Makes the String to the Hash 
-    passed_img_hash = h.hexdigest().strip()#Prints the Hash
-    print("Passed image hash:", passed_img_hash)
-    
-    img_hash_to_room = livingroom_hash_to_room
-    if room_type == "bedroom":
-        img_hash_to_room = bedroom_hash_to_room
-
-    if img_hash_to_room.get(passed_img_hash) is None:
-        print("No such room in the database. Choosing random one.")
-        _, room_id = random.choice(list(img_hash_to_room.items()))
-    else:
-        print("Room found in the database.")
-        room_id = img_hash_to_room[passed_img_hash]
+def visualize_3d_model(room_layout, style, n_double_bed, n_wardrobe, n_pendant_lamp, n_ceiling_lamp, n_tv_stand, n_nightstand, room_type):
+    print("Room type: ", room_type)
+    pil_image = Image.fromarray(room_layout)
+    binary_data = pil_image.tobytes()
     print(f"Style: {style}")
 
     objects_dict = {
@@ -77,7 +55,11 @@ def visualize_3d_model(room_layout, room_type, style, n_double_bed, n_wardrobe, 
     print(f"Objects: {objects_str}")
 
     filepath = f"created_models/{int(time.time())}.glb"
-    if download_3d_model(save_path=filepath, style=style, room_type=room_type, room_id=room_id, objects=objects_str):
+    if download_3d_model(
+        save_path=filepath, style=style, room_type=room_type,
+        objects=objects_str, room_layout=binary_data, 
+        layout_width=pil_image.width, layout_height=pil_image.height
+    ):
         return gr.Model3D(filepath)
     else:
         return "Failed to download 3D model."
@@ -86,14 +68,16 @@ def visualize_3d_model(room_layout, room_type, style, n_double_bed, n_wardrobe, 
 with gr.Blocks() as demo:
     gr.Markdown('<h1 style="text-align: center;"> 3D Rooms Generator</h1>')
     with gr.Tab("Bedroom"):
-        room_type = "bedroom"
         with gr.Column():
+            room_type = "bedroom"
             room_layout = gr.Image()
             style = gr.Dropdown(['Light Luxury', 'Modern', 'Japanese', 
                         'Southeast Asia', 'Nordic', 'Korean', 
                         'Mediterranean', 'New Chinese', 'Vintage/Retro', 
                         'Neoclassical', 'American Country', 
-                        'Ming Qing', 'Minimalist', 'Industrial', 'No style'], value="No style", label="Style")
+                        'Ming Qing', 'Minimalist', 'Industrial', 'No style'], 
+                        value="No style", 
+                        label="Style")
             with gr.Accordion("Set objects"):
                 n_double_bed = gr.Slider(-1, 5, step=1, label="double_bed")
                 n_wardrobe = gr.Slider(-1, 5, step=1, label="wardrobe")
@@ -106,7 +90,7 @@ with gr.Blocks() as demo:
         
         generate_bedroom_button = gr.Button("Generate Bedroom")
 
-    with gr.Tab("Living Room") as room_type:
+    with gr.Tab("Living Room"):
         room_type = "living_room"
         with gr.Row():
             image_input = gr.Image()
@@ -115,8 +99,16 @@ with gr.Blocks() as demo:
 
         generate_living_room_button = gr.Button("Generate Living Room")
 
-    generate_bedroom_button.click(visualize_3d_model, inputs=[room_layout, style, n_double_bed, n_wardrobe, n_pendant_lamp, n_ceiling_lamp, n_tv_stand, n_nightstand], outputs=output_model)
-    generate_living_room_button.click(visualize_3d_model, inputs=[room_layout, style, n_double_bed, n_wardrobe, n_pendant_lamp, n_ceiling_lamp, n_tv_stand, n_nightstand], outputs=output_model)
+    generate_bedroom_button.click(
+        visualize_3d_bedroom, 
+        inputs=[room_layout, style, n_double_bed, n_wardrobe, n_pendant_lamp, n_ceiling_lamp, n_tv_stand, n_nightstand], 
+        outputs=output_model,
+    )
+    generate_living_room_button.click(
+        visualize_3d_livingroom, 
+        inputs=[room_layout, style, n_double_bed, n_wardrobe, n_pendant_lamp, n_ceiling_lamp, n_tv_stand, n_nightstand], 
+        outputs=output_model,
+    )
 
 if __name__ == "__main__":
     demo.launch(share=True)
